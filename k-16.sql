a762ceece09e9ee1db0c6af20b4c3f665fb12048
Jryke <rykerson@gmail.com>
2019-09-19 13:02:54 +0700

    160103

--- k-16.sql
+++ k-16.sql
@@ -5,3 +5,7 @@ VALUES ('Entire Villa')
 -- 160102
 INSERT INTO users (name, email)
 VALUES ('John Smith', 'john@email.com')
+
+-- 160103
+INSERT INTO places (title, price, rating, city, type, host)
+VALUES ('Amazing Villa by the Ocean', 150, 5, 'Phuket', 1, 2)
